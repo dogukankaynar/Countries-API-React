8d557a189eb5f94dd03fdcf0c69ad367a84e27a7
dogukankaynar <dogukankaynar98@gmail.com>
2022-04-30 19:14:32 +0300

    change fetch parameter with axios

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import './App.css';
 import {useEffect,useState} from 'react'
+import axios from 'axios'
 
 function App() {
   const [countries,setCountries] =useState([])
-  useEffect(() =>{
-    fetch('https://restcountries.com/v2/all')
-    .then(response=>response.json())
-    .then(data=>setCountries(data))
+  // useEffect(() =>{
+  //   fetch('https://restcountries.com/v2/all')
+  //   .then(response=>response.json())
+  //   .then(data=>setCountries(data))
+  //   .catch(error=>console.log(error))
+
+  // },[])
+  useEffect(() => {
+    axios.get('https://restcountries.com/v2/all')
+    .then(response=>setCountries(response.data))
     .catch(error=>console.log(error))
-
   },[])
-
     console.log(countries)
   return (
     <div className="App">
